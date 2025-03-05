cbbc02c626da950ca1f9faba9d32553c4bc4f86c
Alex Archambault <alexandre.archambault@gmail.com>
2025-03-05 11:52:06 +0100

    Setup publishing on CI

--- bsp4j/params.java
+++ bsp4j/params.java
@@ -2,6 +2,19 @@
 //> using dep "com.google.code.gson:gson:2.11.0"
 //> using dep "org.eclipse.lsp4j:org.eclipse.lsp4j.generator:0.23.1"
 //> using dep "org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:0.23.1"
-//> using publish.organization "ch.epfl.scala"
+
+//> using publish.organization "io.github.plasmon-scala"
 //> using publish.name "bsp4j"
-//> using publish.version "2.2.0-M3"
+//> using publish.ci.computeVersion "git:tag:../"
+//> using publish.computeVersion "git:tag:../"
+//> using publish.ci.repository "central"
+//> using publish.license "Apache-2.0"
+//> using publish.url "https://github.com/plasmon-scala/build-server-protocol"
+//> using publish.versionControl "github:plasmon-scala/build-server-protocol"
+//> using publish.developer "Alex Archambault||https://github.com/alexarchambault"
+
+//> using publish.ci.user env:PUBLISH_USER
+//> using publish.ci.password env:PUBLISH_PASSWORD
+//> using publish.ci.publicKey env:PUBLISH_PUBLIC_KEY
+//> using publish.ci.secretKey env:PUBLISH_SECRET_KEY
+//> using publish.ci.secretKeyPassword env:PUBLISH_SECRET_KEY_PASSWORD
